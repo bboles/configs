29e1aa31205c601387d96cdd3d211a532b643d10
Brandon Boles <brandon.boles@tylertech.com>
2023-06-22 12:52:12 -0700

    Remove coc, add fzf-lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -38,9 +38,13 @@ return require('packer').startup(function(use)
     use { 'williamboman/mason-lspconfig.nvim'}
     use { 'neovim/nvim-lspconfig' }
     use { 'junegunn/fzf' }
-    use { 'neoclide/coc.nvim', branch = 'release', config = [[require('config.coc')]] }
     use { 'ryanoasis/vim-devicons' }
     use { 'lukas-reineke/indent-blankline.nvim', config = [[require('config.indent-blankline')]] }
+    use { 'ibhagwan/fzf-lua',
+      -- optional for icon support
+      requires = { 'nvim-tree/nvim-web-devicons' },
+      config = [[require('config.fzf-lua')]]
+    }
 
     -- Autocompletion things.
     use { 'hrsh7th/nvim-cmp', config = [[require('config.nvim-cmp')]] }    
