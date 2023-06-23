6b431b8248f4d9e662f7587de2feb8ee807d979d
Brandon Boles <brandon.boles@tylertech.com>
2023-06-23 15:52:13 -0700

    Add more options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,6 +4,8 @@ vim.opt.clipboard = 'unnamedplus'   -- use system clipboard
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
 vim.g.netrw_liststyle = 1           -- show timestamps in listing
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
 
 -- Tab
 vim.opt.tabstop = 2                 -- number of visual spaces per TAB
@@ -18,12 +20,22 @@ vim.opt.cursorline = true           -- highlight cursor line underneath the curs
 vim.opt.splitbelow = false          -- open new vertical split top
 vim.opt.splitright = true           -- open new horizontal splits right
 vim.opt.termguicolors = true        -- enable 24-bit RGB color in the TUI
+vim.opt.colorcolumn = "80"
 
 -- Searching
 vim.opt.incsearch = true            -- search as characters are entered
 vim.opt.hlsearch = true             -- highlight matches
 vim.opt.ignorecase = true           -- ignore case in searches by default
 vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
+-- This makes highlights disappear as soon as you press any movement key.
+vim.on_key(function(char)
+  if vim.fn.mode() == "n" then
+    local new_hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, vim.fn.keytrans(char))
+    if vim.opt.hlsearch:get() ~= new_hlsearch then vim.opt.hlsearch = new_hlsearch end
+  end
+end, vim.api.nvim_create_namespace "auto_hlsearch")
+
+
 
 -- These bits cause a message to be displayed if the file is modified outside of nvim.
 if vim.g.CheckUpdateStarted == nil then
