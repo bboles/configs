5487741686c861bfbc14bbfc1a1cb3b045c6349e
Brandon Boles <brandon.boles@tylertech.com>
2023-08-07 10:57:54 -0700

    Add diagnostic info

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -22,7 +22,7 @@ require('lualine').setup {
     -- +-------------------------------------------------+
     sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_b = { 'branch', 'diff', { 'diagnostics', sources = { 'nvim_diagnostic', 'coc' }}},
         lualine_c = { 'filename' },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
