fc58e1ed5210ce1788f1d7d04aa78e2451c269f4
Brandon Boles <brandon.boles@tylertech.com>
2023-10-04 17:39:03 -0700

    Use noice

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,25 @@
+local M = {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+}
+
+M.config = function()
+  require("noice").setup {
+    presets = {
+      command_palette = true,
+    }
+  }
+end
+
+return M
