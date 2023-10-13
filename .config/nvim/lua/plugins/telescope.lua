10a8032313dd240f6fad35d4d16c13f794fc0b82
Brandon Boles <brandon.boles@tylertech.com>
2023-10-12 17:27:10 -0700

    Add plugin

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+local M = {
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+}
+
+M.config = function()
+  require('telescope').setup({
+    -- Use "dropdown" theme for all pickers.
+    defaults = require('telescope.themes').get_dropdown { },
+  })
+
+  vim.api.nvim_set_keymap('n', '<Leader>fF', ':Telescope find_files<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope git_files<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<Leader>fG', ':Telescope grep_string<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<Leader>fh', ':Telescope help_tags<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<Leader>fm', ':Telescope man_pages<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<Leader>f?', ':Telescope keymaps<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope git_branches<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<Leader>fs', ':Telescope git_stash<CR>', { silent = true })
+end
+
+
+return M
