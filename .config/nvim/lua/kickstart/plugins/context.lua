00313622bd36e9b719c3f9d497a463be33e6df53
Brandon Boles <brandon.boles@clinicalink.com>
2026-07-30 12:48:28 -0700

    Hide context.vim while telescope window is active

--- .config/nvim/lua/kickstart/plugins/context.lua
+++ .config/nvim/lua/kickstart/plugins/context.lua
@@ -1,5 +1,25 @@
 local M = {
   'wellle/context.vim',
+  config = function()
+    -- Hide the context.vim line while a Telescope window is open, then
+    -- restore it once the picker closes.
+    local group = vim.api.nvim_create_augroup('context_telescope', { clear = true })
+    vim.api.nvim_create_autocmd('FileType', {
+      group = group,
+      pattern = 'TelescopePrompt',
+      callback = function(args)
+        vim.cmd 'ContextDisable'
+        vim.api.nvim_create_autocmd({ 'BufWipeout', 'BufDelete' }, {
+          group = group,
+          buffer = args.buf,
+          once = true,
+          callback = function()
+            vim.cmd 'ContextEnable'
+          end,
+        })
+      end,
+    })
+  end,
 }
 
 return M
